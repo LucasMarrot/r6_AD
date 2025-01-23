5abb3e6d51fd9467b63b74dbb6aa58d5ef8c9cdd
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-01-23 11:33:40 +0100

    TP1 - EXO2 : Refactor Application (rename to english)

--- tdtp1/carApp/src/main/java/Application.java
+++ tdtp1/carApp/src/main/java/Application.java
@@ -4,15 +4,15 @@ public class Application {
         final int speedMax = 80;
         final int speedMin = 30;
         final String endMsg = "End";
-        Voiture maVoiture = new Voiture("Sedan", "Bleu");
-        Conducteur moi = new Conducteur("John", myAge);
+        Car myCar = new Car("Sedan", "Bleu");
+        Driver me = new Driver("John", myAge);
 
-        moi.demarrerVoiture(maVoiture);
-        maVoiture.accelerer();
-        moi.changerVitesse(maVoiture, speedMax);
-        maVoiture.ralentir();
-        moi.changerVitesse(maVoiture, speedMin);
-        moi.arreterVoiture(maVoiture);
+        me.demarrerVoiture(myCar);
+        myCar.accelerer();
+        me.changerVitesse(myCar, speedMax);
+        myCar.ralentir();
+        me.changerVitesse(myCar, speedMin);
+        me.arreterVoiture(myCar);
         System.out.println(endMsg);
     }
 }
